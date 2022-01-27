885ea67fb8e68a2cd2cab32fc7cbdbf850677d8d
thebestandgreatest <thebestandgreatest040404@gmail.com>
2022-01-27 14:19:50 -0700

    pain

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,10 +3,13 @@ using Godot;
 
 public class Player : KinematicBody2D
 {
+    [Signal]
+    delegate void ShootBullet(double x, double y, double rotation, string type);
+    
     // load values from file or server for custom games
     private const int TankSpeed = 100;
     private const double TurretRotateSpeed = 1;
-    private const double BodyRotateSpeed = 1;
+    private const double BodyRotateSpeed = 2;
 
     private Vector2 _velocity;
 
@@ -24,14 +27,13 @@ public class Player : KinematicBody2D
 
     public override void _PhysicsProcess(float delta)
     {
-        // bug if tank moves turret rotation breaks
         GetInput();
-        //Animate();
+        Animate();
         _velocity = MoveAndSlide(_velocity);
 
         //turret angle code
-        double mouseAngle = Math.Round(Mathf.Rad2Deg(_tankTurret.GlobalPosition.AngleToPoint(GetLocalMousePosition()))) - 90;
-        double turretAngle = (float) Math.Round(_tankTurret.RotationDegrees);
+        double mouseAngle = Math.Round(Mathf.Rad2Deg(_tankTurret.GlobalPosition.AngleToPoint(GetGlobalMousePosition()))) - 90;
+        double turretAngle = (float) Math.Round(_tankTurret.GlobalRotationDegrees);
         double angleDifference = AngleDifference(mouseAngle, turretAngle);
         if (angleDifference > 1)
         {
@@ -42,39 +44,54 @@ public class Player : KinematicBody2D
             turretAngle += TurretRotateSpeed;
         }
 
-        //_rotationLabel.Text = "X Velocity: " + _velocity.x + " Y Velocity:" + _velocity.y + " Tankbody Rotation: " + _tankBody.RotationDegrees + " Vector Rotation: " +Mathf.Rad2Deg(_velocity.Angle());
-        _tankTurret.RotationDegrees = (float) Math.Round(turretAngle);
+        //_rotationLabel.Text = "Target Angle: " + mouseAngle + ", Current Angle: " + turretAngle;
+        _tankTurret.GlobalRotationDegrees = (float) Math.Round(turretAngle);
     }
 
     private void GetInput()
     {
         //todo shooting
+
+        if (Input.IsActionPressed("fire"))
+        {
+            EmitSignal("ShootBullet", (double) _tankTurret.GlobalPosition.x, (double) _tankTurret.GlobalPosition.y,
+                (double) _tankTurret.GlobalRotationDegrees, "basic");
+        }
         _velocity = new Vector2();
         _velocity = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down") * TankSpeed;
     }
 
     private void Animate()
     {
-        double angleDifference = AngleDifference(Mathf.Rad2Deg(_velocity.Angle()) + 90, _tankBody.RotationDegrees);
+        double angleDifference = AngleDifference(Mathf.Rad2Deg(_velocity.Angle()) - 90, _tankBody.RotationDegrees);
         double bodyAngle = _tankBody.RotationDegrees;
-        
-        if (angleDifference > 0)
-        {
-            bodyAngle += BodyRotateSpeed;
-        }
-        else if (angleDifference < 0)
-        {
-            bodyAngle -= BodyRotateSpeed;
-        }
 
-        _tankBody.RotationDegrees = (float) bodyAngle;
+        if (_velocity != Vector2.Zero)
+        {
+            if (angleDifference > 0)
+            {
+                bodyAngle += BodyRotateSpeed;
+            }
+            else if (angleDifference < 0)
+            {
+                bodyAngle -= BodyRotateSpeed;
+            }
+
+            _tankBody.RotationDegrees = (float) bodyAngle;
+        }
     }
 
-private static double AngleDifference(double testAngle, double currentAngle)
+    private static double AngleDifference(double testAngle, double currentAngle)
     {
         // thank you random person on stack overflow!
         // Takes two angles in degrees and compares the distance between the angles, works for all angles including those outside of [-360,360]
         double diff = (currentAngle - testAngle + 180) % 360 - 180;
         return diff < -180 ? diff + 360 : diff;
+    }
+
+    private void _on_player_ShootBullet(double x, double y, double rotation, string type)
+    {
+        var bulletInstance = _bullet.Instance();
+        AddChild(bulletInstance);
     }
 }
