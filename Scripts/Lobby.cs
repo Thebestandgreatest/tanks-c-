6af724e7a9cbe82db4baba313b22dbaedb2bb0ad
thebestatschool <thebestandgreatest040404@gmail.com>
2022-05-11 14:23:21 -0600

    Host correctly updates with list of player names
    clients dont though

--- Scripts/Lobby.cs
+++ Scripts/Lobby.cs
@@ -11,8 +11,8 @@ public class Lobby : Panel
 	private static Dictionary<int, string> _players = new Dictionary<int, string>();
 	private static Dictionary<int, bool> _playersAlive = new Dictionary<int, bool>();
 
-	[Puppet] private Dictionary<int, string> PuppetPlayers = new Dictionary<int, string>();
-	[Puppet] private Dictionary<int, bool> PuppetPlayersAlive = new Dictionary<int, bool>();
+	[PuppetSync] private Dictionary<int, string> _puppetPlayers = new Dictionary<int, string>();
+	[PuppetSync] private Dictionary<int, bool> _puppetPlayersAlive = new Dictionary<int, bool>();
 
 	private PackedScene _player;
 	private Node2D _world;
@@ -93,7 +93,8 @@ public class Lobby : Panel
 		OS.SetWindowTitle("Server");
 		Hide();
 		_network.CreateServer();
-		InstancePlayer(GetTree().GetNetworkUniqueId(), _name.Text);
+		InstancePlayer(GetTree().GetNetworkUniqueId());
+		RpcId(1, nameof(AddPlayer), GetTree().GetNetworkUniqueId(), _name.Text);
 		PreStartGame();
 	}
 
@@ -152,25 +153,22 @@ public class Lobby : Panel
 	private void PlayerConnected(int id)
 	{
 		Console.WriteLine($"Player {id} connected");
-		InstancePlayer(id, "");
+		InstancePlayer(id);
 	}
 	
 	private void ConnectedToServer()
 	{
-		InstancePlayer(GetTree().GetNetworkUniqueId(), _name.Text);
+		InstancePlayer(GetTree().GetNetworkUniqueId());
+		RpcId(1, nameof(AddPlayer), GetTree().GetNetworkUniqueId(), _name.Text);
 		Rpc(nameof(RefreshLobby));
 	}
 	
-	private void InstancePlayer(int id, string name)
+	private void InstancePlayer(int id)
 	{
 		GetTree().Paused = true;
-		if (IsNetworkMaster())
-		{
-			Rpc(nameof(AddPlayer), id, name);
-		}
 		
 		Node2D playerInstance =
-			Global.InstanceNodeAtLocation(_player, _world, new Vector2((float) GD.RandRange(-1700, 1700), (float) GD.RandRange(-1400, 1600)));
+		Global.InstanceNodeAtLocation(_player, _world, new Vector2((float) GD.RandRange(-17, 17) * 100, (float) GD.RandRange(-14, 16) * 100));
 		playerInstance.Name = id.ToString();
 		playerInstance.SetNetworkMaster(id);
 		GetTree().Paused = true;
@@ -182,27 +180,40 @@ public class Lobby : Panel
 		RefreshLobby();
 	}
 
-	[RemoteSync]
+	[Sync]
 	private void AddPlayer(int id, string name)
 	{
 		_players.Add(id, name);
 		_playersAlive.Add(id, true);
-		if (IsNetworkMaster())
+	}
+	
+	[Sync]
+	private void RefreshLobby()
+	{
+		if (GetTree().GetNetworkUniqueId() == 1)
 		{
-			Rset(nameof(PuppetPlayers), _players);
-			Rset(nameof(PuppetPlayersAlive), _playersAlive);
+			Rset(nameof(_puppetPlayers), _players);
+			Rset(nameof(_puppetPlayersAlive), _playersAlive);
 		}
 		else
 		{
-			_players = PuppetPlayers;
-			_playersAlive = PuppetPlayersAlive;	
+			_players = _puppetPlayers;
+			_playersAlive = _puppetPlayersAlive;
 		}
-	}
+		
+		foreach (KeyValuePair<int, string> variable in _players)
+		{
+			Console.WriteLine(variable);
+		}
+
+		foreach (KeyValuePair<int, string> variable in _puppetPlayers)
+		{
+			Console.WriteLine(variable);
+		}
 	
-	[RemoteSync]
-	private void RefreshLobby()
-	{
-		Dictionary<int, string> playerlist = IsNetworkMaster() ? _players : PuppetPlayers;
+		Dictionary<int, string> playerlist = IsNetworkMaster() ? _players : _puppetPlayers;
+		
+		_teamAList.Clear();
 		
 		foreach (KeyValuePair<int, string> player in playerlist)
 		{
